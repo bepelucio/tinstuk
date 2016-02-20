753a0a92203a48d844cfd24e0d514300918579af
Giulia Pizzignacco <be.giu@hotmail.com>
2016-02-20 20:41:00 +0000

    Omniauth Installed

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], { :scope => 'user_location, user_birthday, user_about_me, email' }
+end
+
+# protect that facebook credentials goes to github repository
